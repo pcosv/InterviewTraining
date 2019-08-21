47211b9f235bb150393975abb70ce27cf76652c1
Paula Vaz <pcosv@cin.ufpe.br>
2019-08-21 13:49:43 -0300

    Initial Commit

---
+++ HackerRankTraining/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HackerRankTraining
+//
+//  Created by Paula Vaz on 21/08/19.
+//  Copyright © 2019 Paula Vaz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
